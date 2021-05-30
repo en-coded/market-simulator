6054cf547bcf61669870210a7879965ea4eec5a8
en-coded <83879346+en-coded@users.noreply.github.com>
2021-05-30 01:47:51 -0400

    More changes to CSS

---
+++ script.js
@@ -0,0 +1,12 @@
+var btc_human = document.getElementById('BTC');
+
+function getRandomInt(2, 5) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+
+
+document.getElementsByClassName('PLAY').onclick = function () {
+console.log(getRandomArbitrary*btc_human) };

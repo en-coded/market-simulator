be66a07adeee28b6b50ea7d2d963ec9ab9ba207c
en-coded <83879346+en-coded@users.noreply.github.com>
2021-05-31 01:12:09 -0400

    Risk and Portfolio calculations

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-var btc_human = document.getElementById('BTC');
 
-function getRandomInt(2, 5) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+
+//Get the risk with BTC
+var btc_risk = function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
+var BTC_RISK = btc_risk(0.5,1);
+//End of (Get the risk with BTC)
 
 
-document.getElementsByClassName('PLAY').onclick = function () {
-console.log(getRandomArbitrary*btc_human) };
+//Get BTC investment from Human
+function myFunction() {
+  var HBTC = document.getElementById("HBTC").value;
+  document.getElementById("demo").innerHTML = HBTC;
+}
+// End of BTC investment from Human
+
+function H_VALUE() {
+  var BTC_RISK = btc_risk(0.5,1);
+  var HBTC = document.getElementById("HBTC").value;
+  var BTC_VALUE = BTC_RISK * HBTC;
+  document.getElementById("demo2").innerHTML = BTC_VALUE;
+  }

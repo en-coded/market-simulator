06279d44e1aec79725fc7f1fda566e3c2a91ebc6
en-coded <83879346+en-coded@users.noreply.github.com>
2021-05-31 18:30:03 -0400

    Updated Portfolio Page

--- script.js
+++ script.js
@@ -44,7 +44,43 @@ function H_VALUE() {
   var HUNI = HUNI * 10000 / 100;
   var HXLM = HXLM * 10000 / 100;
   var HADA = HADA * 10000 / 100;
-  var CASH = 10000 - HBTC - HETH - HXRP - HBNB - HDOGE - HDOT - HLINK - HUNI - HXLM - HADA;
-  var PORT_VALUE = btc_risk_val * HBTC + eth_risk_val * HETH + xrp_risk_val * HXRP + bnb_risk_val * HBNB + doge_risk_val * HDOGE + dot_risk_val * HDOT + link_risk_val * HLINK + uni_risk_val * HUNI + xlm_risk_val * HXLM + ada_risk_val * HADA + CASH;
-  document.getElementById("portfolio").innerHTML = PORT_VALUE;
+  var HCASH = 10000 - HBTC - HETH - HXRP - HBNB - HDOGE - HDOT - HLINK - HUNI - HXLM - HADA;
+  var HPORT_VALUE = btc_risk_val * HBTC + eth_risk_val * HETH + xrp_risk_val * HXRP + bnb_risk_val * HBNB + doge_risk_val * HDOGE + dot_risk_val * HDOT + link_risk_val * HLINK + uni_risk_val * HUNI + xlm_risk_val * HXLM + ada_risk_val * HADA + HCASH;
+  document.getElementById("hportfolio").innerHTML = HPORT_VALUE;
   }
+
+  function S_VALUE() {
+    var btc_risk_val = risk(0.95,1.1);
+    var eth_risk_val = risk(0.9,1.2);
+    var xrp_risk_val = risk(0.75,1.25);
+    var bnb_risk_val = risk(0.95,1.25);
+    var doge_risk_val = risk(0.95,1.1);
+    var dot_risk_val = risk(0.8,1.2);
+    var link_risk_val = risk(0.45,1.0);
+    var uni_risk_val = risk(0.75,1.25);
+    var xlm_risk_val = risk(0.95,1.1);
+    var ada_risk_val = risk(0.8,1.2);
+    var SBTC = document.getElementById("SBTC").value;
+    var SETH = document.getElementById("SETH").value;
+    var SXRP = document.getElementById("SXRP").value;
+    var SBNB = document.getElementById("SBNB").value;
+    var SDOGE = document.getElementById("SDOGE").value;
+    var SDOT = document.getElementById("SDOT").value;
+    var SLINK = document.getElementById("SLINK").value;
+    var SUNI = document.getElementById("SUNI").value;
+    var SXLM = document.getElementById("SXLM").value;
+    var SADA = document.getElementById("SADA").value;
+    var SBTC = SBTC * 10000 / 100;
+    var SETH = SETH * 10000 / 100;
+    var SXRP = SXRP * 10000 / 100;
+    var SBNB = SBNB * 10000 / 100;
+    var SDOGE = SDOGE * 10000 / 100;
+    var SDOT = SDOT * 10000 / 100;
+    var SLINK = SLINK * 10000 / 100;
+    var SUNI = SUNI * 10000 / 100;
+    var SXLM = SXLM * 10000 / 100;
+    var SADA = SADA * 10000 / 100;
+    var SCASH = 10000 - SBTC - SETH - SXRP - SBNB - SDOGE - SDOT - SLINK - SUNI - SXLM - SADA;
+    var SPORT_VALUE = btc_risk_val * SBTC + eth_risk_val * SETH + xrp_risk_val * SXRP + bnb_risk_val * SBNB + doge_risk_val * SDOGE + dot_risk_val * SDOT + link_risk_val * SLINK + uni_risk_val * SUNI + xlm_risk_val * SXLM + ada_risk_val * SADA + SCASH;
+    document.getElementById("sportfolio").innerHTML = SPORT_VALUE;
+    }

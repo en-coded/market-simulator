2520e5b712a468f851653a34ff5a30825bc49ba1
en-coded <83879346+en-coded@users.noreply.github.com>
2021-05-31 02:57:37 -0400

    Updates for Portfolio changes

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 
 
 //Get the risk with BTC
-var btc_risk = function getRandomArbitrary(min, max) {
+var risk = function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-var BTC_RISK = btc_risk(0.5,1);
 //End of (Get the risk with BTC)
 
-
 //Get BTC investment from Human
-function myFunction() {
+function btc_input() {
   var HBTC = document.getElementById("HBTC").value;
   document.getElementById("demo").innerHTML = HBTC;
 }
 // End of BTC investment from Human
 
 function H_VALUE() {
-  var BTC_RISK = btc_risk(0.5,1);
+  var btc_risk_val = risk(0.95,1.1);
+  var eth_risk_val = risk(0.9,1.2);
+  var xrp_risk_val = risk(0.75,1.25);
+  var bnb_risk_val = risk(0.95,1.25);
+  var doge_risk_val = risk(0.95,1.1);
+  var dot_risk_val = risk(0.8,1.2);
+  var link_risk_val = risk(0.45,1.0);
+  var uni_risk_val = risk(0.75,1.25);
+  var xlm_risk_val = risk(0.95,1.1);
+  var ada_risk_val = risk(0.8,1.2);
   var HBTC = document.getElementById("HBTC").value;
-  var BTC_VALUE = BTC_RISK * HBTC;
-  document.getElementById("demo2").innerHTML = BTC_VALUE;
+  var HETH = document.getElementById("HETH").value;
+  var HXRP = document.getElementById("HXRP").value;
+  var HBNB = document.getElementById("HBNB").value;
+  var HDOGE = document.getElementById("HDOGE").value;
+  var HDOT = document.getElementById("HDOT").value;
+  var HLINK = document.getElementById("HLINK").value;
+  var HUNI = document.getElementById("HUNI").value;
+  var HXLM = document.getElementById("HXLM").value;
+  var HADA = document.getElementById("HADA").value;
+  var HBTC = HBTC * 10000 / 100;
+  var HETH = HETH * 10000 / 100;
+  var HXRP = HXRP * 10000 / 100;
+  var HBNB = HBNB * 10000 / 100;
+  var HDOGE = HDOGE * 10000 / 100;
+  var HDOT = HDOT * 10000 / 100;
+  var HLINK = HLINK * 10000 / 100;
+  var HUNI = HUNI * 10000 / 100;
+  var HXLM = HXLM * 10000 / 100;
+  var HADA = HADA * 10000 / 100;
+  var CASH = 10000 - HBTC - HETH - HXRP - HBNB - HDOGE - HDOT - HLINK - HUNI - HXLM - HADA;
+  var PORT_VALUE = btc_risk_val * HBTC + eth_risk_val * HETH + xrp_risk_val * HXRP + bnb_risk_val * HBNB + doge_risk_val * HDOGE + dot_risk_val * HDOT + link_risk_val * HLINK + uni_risk_val * HUNI + xlm_risk_val * HXLM + ada_risk_val * HADA + CASH;
+  document.getElementById("portfolio").innerHTML = PORT_VALUE;
   }
